1f035372ade623b77e247e6e75a4a645ceedc159
HappyBananaKK <alefeltrin.af@gmail.com>
2023-12-13 22:35:57 +0100

    grafici dinamici, implementazione tabella per accessibilita'

--- resources/javascript/investments_charts.js
+++ resources/javascript/investments_charts.js
@@ -1,6 +1,80 @@
-window.onload = function () {
+var dataSeries =  [
+    {        
+        type: "line",
+        indexLabelFontSize: 16,
+        dataPoints: [
+            {y: 5},
+            { y: 10},
+            { y: 30},
+            { y: 5},
+            { y: 10},
+            { y: 15},
+            { y: 20},
+            { y: 10},
+            { y: 5},
+            { y: 5},
+            { y: 20},
+            { y: 20},
+        ]
+    },
+    {        
+        type: "line",
+        indexLabelFontSize: 16,
+        dataPoints: [
+            {y: 25},
+            { y: 5},
+            { y: 20},
+            { y: 20},
+            { y: 40},
+            { y: 50},
+            { y: 40},
+            { y: 40},
+            { y: 10},
+            { y: 20},
+            { y: 30},
+            { y: 20},
+        ]
+    },{        
+        type: "line",
+        indexLabelFontSize: 16,
+        dataPoints: [
+            {y: 10},
+            { y: 10},
+            { y: 30},
+            { y: 5},
+            { y: 10},
+            { y: 15},
+            { y: 20},
+            { y: 10},
+            { y: 5},
+            { y: 5},
+            { y: 20},
+            { y: 20},
+        ]
+    },{        
+        type: "line",
+        indexLabelFontSize: 16,
+        dataPoints: [
+            {y: 35},
+            { y: 10},
+            { y: 30},
+            { y: 5},
+            { y: 10},
+            { y: 15},
+            { y: 20},
+            { y: 10},
+            { y: 5},
+            { y: 5},
+            { y: 20},
+            { y: 20},
+        ]
+    }
+];
 
-    var chart = new CanvasJS.Chart("graph", {
+window.onload = function () {
+    
+
+    window.chart = new CanvasJS.Chart("graph", {
         animationEnabled: true,
         theme: "light2",
         axisY: {
@@ -13,24 +87,25 @@ window.onload = function () {
         axisX: {
             title: "Month",
         },
-        data: [{        
-            type: "line",
-            indexLabelFontSize: 16,
-            dataPoints: [
-                {y: 450},
-                { y: 414},
-                { y: 520},
-                { y: 460},
-                { y: 450},
-                { y: 500},
-                { y: 480},
-                { y: 480},
-                { y: 410},
-                { y: 500},
-                { y: 480},
-                { y: 510},
-            ]
-        }]
+        data: [dataSeries[3]] 
     });
     chart.render();
 }
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    // prendo l'elemento selezionato
+    var select = document.querySelector('.list_years');
+
+    select.addEventListener('change', function() {
+        // mostro il grafico che corrisponde all'anno selezionato
+        chart.options.data[0] = dataSeries[select.value - 2020];
+        chart.render();
+    });
+});
+
+
+
+
+  
