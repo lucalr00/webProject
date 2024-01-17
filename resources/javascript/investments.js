a2bf916c679b7e669a76c34ef1eb5b3f16304bb7
HappyBananaKK <alefeltrin.af@gmail.com>
2024-01-17 16:06:58 +0100

    test che non va

--- resources/javascript/investments.js
+++ resources/javascript/investments.js
@@ -1,4 +1,4 @@
-
+/*
 document.addEventListener('DOMContentLoaded', function() {
     // prendo l'elemento selezionato
     var select = document.querySelector('.list_years');
@@ -10,12 +10,14 @@ document.addEventListener('DOMContentLoaded', function() {
         p = document.getElementById(i.toString());
         if(p) {
             if(i.toString() === select.value) {
-                p.style.display = 'block';
+                if (p.className === "description") {
+                    p.className += " active";          
             } else {
-                p.style.display = 'none';
+                p.className += "description";
             }
         }
     }
+}
 
     select.addEventListener('change', function() {
         // nascondi l'elemento precedente
@@ -26,6 +28,43 @@ document.addEventListener('DOMContentLoaded', function() {
         saved_value = this.value;
         if(selectedP) {
             selectedP.style.display = 'block';
+        }
+    });
+});
+*/
+document.addEventListener("DOMContentLoaded", () => {
+    // prendo l'elemento selezionato
+    var select = document.querySelector('.list_years');
+
+    var saved_value = select.value; 
+    // nascondo tutti gli elementi tranne quello selezionato
+     var t;
+    for(var i = 2020; i <= 2050; i++){
+        t = document.getElementById(i.toString());
+        if(t) {
+            if(i.toString() === select.value) {
+                if (t.className === "description") {
+                    t.className += " active";          
+            } else {
+                t.className += "description";
+            }
+        }
+    }
+}
+
+    select.addEventListener('change', function() {
+        // nascondi l'elemento precedente
+        document.getElementById(saved_value).style.display = 'none';
+
+        // mostra l'elemento selezionato
+        var selectedP = document.getElementById(this.value);
+        saved_value = this.value;
+        if(selectedP) {
+            if (t.className === "description") {
+                t.className += " active";          
+        } else {
+            t.className += "description";
+        }
         }
     });
 });
