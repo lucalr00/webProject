fd527084fab33352db0b1ed17c12c95d0ca9ee4d
Raulp01 <raul.pianon@oulook.com>
2024-01-30 22:58:58 +0100

    Accessibilità projects.js

--- resources/javascript/projects.js
+++ resources/javascript/projects.js
@@ -3,6 +3,15 @@ document.addEventListener("DOMContentLoaded", () => {
 	startScript();
 });
 
+document.addEventListener('keydown', function(event) {
+	if (event.keyCode == 37) {
+		plusSlides(-1);
+	}
+	else if (event.keyCode == 39) {
+		plusSlides(1);
+	}
+});
+
 let slideIndex = 1;
 
 function startScript() {
@@ -32,14 +41,19 @@ function showSlide(currentIndex)
     for(index = 0; index < slide.length; index++)
     {
         slide[index].className ="slide_content hidden";
+        slide[index].tabIndex = 0;
     }
 
     for (index = 0; index < dots.length; index++) {
-        dots[index].className = "dot disable"
+        dots[index].className = "dot disable";
+        dots[index].setAttribute('aria-selected', 'false');
     }
 
-    slide[slideIndex - 1].className = "slide_content visible"
+    slide[slideIndex - 1].className = "slide_content visible";
     dots[slideIndex - 1].className = "dot enable";
+    dots[slideIndex - 1].setAttribute('aria-selected', 'true');
+    slide[slideIndex - 1].tabIndex = -1;
+    slide[slideIndex - 1].focus();
 }
 
 function currentSlide(currentIndex) {
